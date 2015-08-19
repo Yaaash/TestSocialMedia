3c82ff003dad9bdf04efcb8bb9485fa90f484e74
yaaash <ykyashikakatyal@gmail.com>
2015-08-19 16:12:57 +0530

    Facebook Integration Phase 1

--- app/src/main/java/com/coppermobile/katyal/yashika/testsocialmedia/MainActivity.java
+++ app/src/main/java/com/coppermobile/katyal/yashika/testsocialmedia/MainActivity.java
@@ -1,17 +1,9 @@
 package com.coppermobile.katyal.yashika.testsocialmedia;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,21 +16,6 @@ public class MainActivity extends AppCompatActivity {
         // setting up activity layout
         setContentView(R.layout.activity_main);
 
-        // Add code to print out the key hash
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "com.facebook.samples.hellofacebook",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-
-        } catch (NoSuchAlgorithmException e) {
-
-        }
     }
 
     @Override
