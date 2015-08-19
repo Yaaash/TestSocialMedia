ef6100a329546b6addbf7d89e2827e23ad19bb21
yaaash <ykyashikakatyal@gmail.com>
2015-08-19 16:11:12 +0530

    Facebook Integration Phase 1

--- app/src/main/java/com/coppermobile/katyal/yashika/testsocialmedia/MainActivity.java
+++ app/src/main/java/com/coppermobile/katyal/yashika/testsocialmedia/MainActivity.java
@@ -1,18 +1,46 @@
 package com.coppermobile.katyal.yashika.testsocialmedia;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // saving current activity instance
         super.onCreate(savedInstanceState);
+
+        // setting up activity layout
         setContentView(R.layout.activity_main);
+
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.facebook.samples.hellofacebook",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
